f2232c66a320c73d0f56d61dc9b2fee7d28d6b4f
AndreyDovban <dovban.andreyy@rambler.ru>
2025-03-19 07:58:15 +0300

    Работа с вебсокетами

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,61 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
+
+// Upgrader is used to upgrade HTTP connections to WebSocket connections.
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool) // Connected clients
+var broadcast = make(chan []byte)            // Broadcast channel
+var mutex = &sync.Mutex{}                    // Protect clients map
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading:", err)
+		return
+	}
+	defer conn.Close()
+
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			mutex.Lock()
+			delete(clients, conn)
+			mutex.Unlock()
+			break
+		}
+		broadcast <- message
+	}
+}
+
+func handleMessages() {
+	for {
+		message := <-broadcast
+
+		mutex.Lock()
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
+		}
+		mutex.Unlock()
+	}
+}
 
 func App() http.Handler {
 
@@ -12,12 +66,11 @@ func App() http.Handler {
 	fileServer := http.FileServer(http.Dir("./dist"))
 	router.Handle("/", fileServer)
 
-	router.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "About Page")
-	})
+	router.HandleFunc("/ws", wsHandler)
+
+	go handleMessages()
 
 	return router
-
 }
 
 func main() {
--- dist/assets/script.js
+++ dist/assets/script.js
@@ -1,3 +1,36 @@
-let but = document.querySelector('#but');
+let ws;
 
-but.onclick = () => console.log('work');
+function connect() {
+	ws = new WebSocket('ws://127.0.0.1:5000/ws');
+
+	ws.onopen = function () {
+		console.log('Connected to WebSocket server');
+	};
+
+	ws.onmessage = function (event) {
+		let messageDisplay = document.getElementById('messages');
+		messageDisplay.innerHTML += `<p>${event.data}</p>`;
+	};
+
+	ws.onclose = function () {
+		console.log('WebSocket connection closed, retrying...');
+		setTimeout(connect, 1000); // Reconnect after 1 second
+	};
+
+	ws.onerror = function (error) {
+		console.error('WebSocket error:', error);
+	};
+}
+
+function sendMessage() {
+	let input = document.getElementById('messageInput');
+	let message = input.value;
+	ws.send(message);
+	input.value = '';
+}
+
+connect();
+
+const but2 = document.querySelector('#but2');
+
+but2.onclick = sendMessage;
